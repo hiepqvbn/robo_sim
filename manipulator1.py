9eb48e1e8685149722fb578517f2e8c747ff70a6
hiep_kbysLAB <nguyen.xuan.hiep.18@shizuoka.ac.jp>
2021-11-11 16:56:13 +0900

    Done manipulator1

--- manipulator1.py
+++ manipulator1.py
@@ -10,18 +10,18 @@ def manipulator1(q, qdot, tau):
     lg1=glb_.l1*0.5
     lg2=glb_.l2*0.5
 
-    M11=glb_.m1*lg1**2+glb_.I1+glb_.m2*(glb_.l1**2+lg2**2 +2*glb_.l1*lg2*cos(theta[1]))+glb_.I2
-    M12=glb_.m2*(lg2**2+glb_.l1*lg2*cos(theta[1]))+glb_.I2
+    M11=glb_.m1*lg1**2+glb_.I1+glb_.m2*(glb_.l1**2+lg2**2 +2*glb_.l1*lg2*cos(theta[1][0]))+glb_.I2
+    M12=glb_.m2*(lg2**2+glb_.l1*lg2*cos(theta[1][0]))+glb_.I2
     M22=glb_.m2*lg2**2+glb_.I2
-    g1 =glb_.m1*glb_.ghat*lg1*cos(theta[1])+glb_.m2*glb_.ghat*(glb_.l1*cos(theta[1])+lg2*cos(theta[0]+theta[1]))
-    g2 =glb_.m2*glb_.ghat*lg2*cos(theta[0]+theta[1])
+    g1 =glb_.m1*glb_.ghat*lg1*cos(theta[0][0])+glb_.m2*glb_.ghat*(glb_.l1*cos(theta[0][0])+lg2*cos(theta[0][0]+theta[1][0]))
+    g2 =glb_.m2*glb_.ghat*lg2*cos(theta[0][0]+theta[1][0])
 
-    h122=-glb_.m2*glb_.l1*lg2*sin(theta[1])
+    h122=-glb_.m2*glb_.l1*lg2*sin(theta[1][0])
     h112=h122
     h211=-h122
 
-    h1 = h122*thetadot[1]**2+2*h112*thetadot[0]*thetadot[1]
-    h2 = h211*thetadot[0]**2
+    h1 = h122*thetadot[1][0]**2+2*h112*thetadot[0][0]*thetadot[1][0]
+    h2 = h211*thetadot[0][0]**2
 
     M=np.array(
         [
@@ -44,17 +44,30 @@ def manipulator1(q, qdot, tau):
         ]
     )
 
-    qddot= np.linalg.inv(M)*(tau-h-g)
+    qddot= np.matmul(np.linalg.inv(M),(tau-h-g))
     print(qddot)
 
-    newq=0
-    newqdot=0
+    newqdot=qdot+qddot*glb_.dt
+    print(newqdot)
+    newq=theta+thetadot*glb_.dt
+    print(newq)
+
     return newq, newqdot
 
 if __name__=="__main__":
     tau=np.array(
         [
-            [1], [1]
+            [0], [0]
         ]
     )
-    print(manipulator1([1, 2],[0, 0],tau))
+    q=np.array(
+        [
+            [-1.4708], [0.2]
+        ]
+    )
+    qdot=np.array(
+        [
+            [0], [0]
+        ]
+    )
+    manipulator1(q,qdot,tau)
